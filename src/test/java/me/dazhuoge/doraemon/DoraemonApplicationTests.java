11bfe2c308b17feacb6710f45aa946244f75c30f
Zhuo Wang <zhuowang@Zhuos-MacBook-Pro.local>
2020-02-22 16:09:19 -0600

    first run

---
+++ src/test/java/me/dazhuoge/doraemon/DoraemonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dazhuoge.doraemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoraemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
